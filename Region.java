91d95d3bf808b92c160a5f2b3324e1e6e6a7d80a
Nate Foss <npfoss@gmail.com>
2018-01-16 16:08:42 -0500

    all the setup stuff

---
+++ Region.java
@@ -0,0 +1,12 @@
+import bc.*;
+import java.util.ArrayList;
+
+public class Region{
+    ArrayList<Tile> tiles;
+    long karbonite;
+
+    public Region(){
+        tiles = new ArrayList<Tile>();
+        karbonite = 0;
+    }
+}
---
+++ Tile.java
@@ -0,0 +1,35 @@
+import bc.*;
+import java.util.HashMap;
+
+public class Tile{
+    int x;
+    int y;
+    boolean isWalkable;
+    long karbonite;
+    Region region;
+
+    int roundLastUpdated;
+    int hittableBy;
+    double possibleDamage;
+    HashMap<MapLocation, Signpost> destToDir;
+
+    public Tile(int ex, int why, boolean walkable, long karb, Region reg){
+        x = ex;
+        y = why;
+        isWalkable = walkable;
+        karbonite = karb;
+        region = reg;
+
+        roundLastUpdated = 0;
+        hittableBy = 0;
+        possibleDamage = 0;
+        destToDir = new HashMap<MapLocation, Signpost>();
+    }
+
+    public void updateKarbonite(long newKarb){
+        if (newKarb != karbonite){
+            region.karbonite += newKarb - karbonite;
+            karbonite = newKarb;
+        }
+    }
+}
