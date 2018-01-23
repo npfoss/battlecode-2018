b9652594cdf52c53de08572ec358334654371796
uvafan <uvafan173@gmail.com>
2018-01-23 10:35:48 -0500

    fix

--- RocketDoNothing.java
+++ RocketDoNothing.java
@@ -12,6 +12,17 @@ public class RocketDoNothing extends RocketManager{
 
     public void update(){
     	for(RocketSquad s : infoMan.rocketSquads) {
+    		boolean didSomething;
+    		while(gc.unit(s.units.get(0)).structureGarrison().size() > 0) {
+    			didSomething = false;
+    			for(Direction dirToUnload : Utils.orderedDirections)
+    				if(gc.canUnload(s.units.get(0), dirToUnload)) {
+    					gc.unload(s.units.get(0), dirToUnload);
+    					didSomething = true;
+    				}
+    			if(!didSomething)
+    				break;
+    		}
     		if(gc.unit(s.units.get(0)).structureGarrison().size() == 0) {
     			gc.disintegrateUnit(s.units.get(0));
     		}
