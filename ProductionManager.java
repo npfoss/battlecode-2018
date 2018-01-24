f8d00ecf3ac4d88851e51b3ac874fe7fd3c6f0ff
Nate <npfoss@gmail.com>
2018-01-23 23:09:09 -0500

    add descriptions before refactor

--- ProductionManager.java
+++ ProductionManager.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 /*
 Production Manager (based on StrategyManager and squad requests)
-BuildOrder Search - possibly change build order based on what happens in game, decide on new build order from strategy manager
-BuildOrder Queue - decides what should be built based on strategy and where to build them
-Creates (empty) WorkerSquad to build factory (workers assigned by WorkerManager)
+
+takes into account squad requests to control factory production.
+does NOT handle worker replication, WorkerManager does that
 */
 public class ProductionManager{
     InfoManager infoMan;
