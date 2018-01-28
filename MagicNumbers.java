1a7d1588e603bec99828bedfd17b5bb01fca91b1
uvafan <uvafan173@gmail.com>
2018-01-28 16:19:35 -0500

    maggie

--- MagicNumbers.java
+++ MagicNumbers.java
@@ -62,7 +62,7 @@ public class MagicNumbers{
     static int FACTORY_COST = 200;
     static int MAX_WORKERS_PER_BUILDING = 6;
     static double MINIMUM_SCORE_TO_STEAL = 10;
-    static int MAX_DIST_TO_STEAL = 20;
+    static int MAX_DIST_TO_STEAL = 50;
     static long KARB_SEPARATION_DISTANCE = 10;
     static Double MIN_SCORE_TO_REPLICATE = 50.0;
     static int FACTORY_SCARED_RADIUS = 120;
